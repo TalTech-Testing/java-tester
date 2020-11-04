f7fc28582138ddd2ba4fd3d1878ff0418982d4a4
envomp <envomp@ttu.ee>
2020-11-04 18:39:56 +0200

    build jar

--- src/test/java/ee/ttu/java/studenttester/core/policy/PolicyTest.java
+++ src/test/java/ee/ttu/java/studenttester/core/policy/PolicyTest.java
@@ -7,28 +7,28 @@ import ee.ttu.java.studenttester.core.models.reports.TestNGReport;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertSame;
 
 public class PolicyTest extends BaseTest {
 
-    @BeforeMethod
-    private void before() {
-        initContext();
-    }
+	@BeforeMethod
+	private void before() {
+		initContext();
+	}
 
-    @Test
-    public void testPolicies() throws Exception {
-        moveResource("/tests/policy/PolicyCheck.java", context.contentRoot);
-        moveResource("/tests/policy/PolicyCheck.java", context.tempRoot);
-        moveResource("/tests/policy/PolicyCheckTest.java", context.tempRoot);
-        moveResource("/tests/policy/PolicyCheckTest.java", context.testRoot);
-        compileAndExpect(RunnerResultType.SUCCESS);
-        runNewTestNG();
+	@Test
+	public void testPolicies() throws Exception {
+		moveResource("/tests/policy/PolicyCheck.java", context.contentRoot);
+		moveResource("/tests/policy/PolicyCheck.java", context.tempRoot);
+		moveResource("/tests/policy/PolicyCheckTest.java", context.tempRoot);
+		moveResource("/tests/policy/PolicyCheckTest.java", context.testRoot);
+		compileAndExpect(RunnerResultType.SUCCESS);
+		runNewTestNG();
 
-        var report = context.results.getResultByType(TestNGReport.class);
-
-        report.testContexts.get(0).unitTests.forEach(singleResult ->
-                assertSame(singleResult.status, TestResultType.PASSED, singleResult.stackTrace));
-    }
+		var report = context.results.getResultByType(TestNGReport.class);
+		System.out.println();
+		report.testContexts.get(0).unitTests.forEach(singleResult ->
+				assertSame(singleResult.status, TestResultType.PASSED, singleResult.stackTrace));
+	}
 
 }
